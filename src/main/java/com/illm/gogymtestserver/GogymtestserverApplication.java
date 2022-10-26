3add5d24b449f925ededf75038c23a664b06b8b8
moonmoon.kim <visitant2@naver.com>
2022-10-26 18:23:09 +0900

    test

---
+++ src/main/java/com/illm/gogymtestserver/GogymtestserverApplication.java
@@ -0,0 +1,13 @@
+package com.illm.gogymtestserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GogymtestserverApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GogymtestserverApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/illm/gogymtestserver/GogymtestserverApplicationTests.java
@@ -0,0 +1,13 @@
+package com.illm.gogymtestserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GogymtestserverApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
